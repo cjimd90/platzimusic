cee59f92f6d4e7b528fbf9815c16ef9c57e623b9
cjimd90 <cjimenezd90@gmail.com>
2019-05-19 09:15:53 -0500

    initial commit

---
+++ src/api/config.js
@@ -0,0 +1,7 @@
+export default {
+    appname: 'platzimusic',
+    apiKey: 'e26a830b7db06407d6ced1fb6c42fb7b',
+    secret: '5dde88a679d0d85f155bb69443a57fd2',
+    registeredTo: 'cjimd90'
+}
+
---
+++ src/components/Artist.vue
@@ -0,0 +1,20 @@
+<template>
+  <li>
+      <h2><a :href="artist.url" target="_black">{{ artist.name }}</a></h2>
+      <img :src="artist.image[2]['#text']">
+  </li>
+</template>
+
+<script>
+export default {
+    name: 'artist',
+    props: ['artist']
+}
+</script>
+
+<style>
+    li {
+        display: block;
+        margin: 10px 0;
+    }
+</style>
